0f7255e6e68df2c9f16df466a7653a562f8dd576
Ivan Eremin <ereminiu@gmail.com>
2023-01-02 20:32:42 +0300

    Edit some files...

--- main.py
+++ main.py
@@ -17,12 +17,7 @@ drawer = draw.Drawer(screen)
 #draw game field
 drawer.draw_tiles()
 
-#write Aboba
 drawer.draw_text('Press Space to show solution')
-
-import sys
-
-# sys.stdout = open('output.txt', 'w')
 
 #generate random sudoku grid
 grid = gen.GridGenerator().get_grid()
@@ -30,17 +25,11 @@ grid = gen.GridGenerator().get_grid()
 #find the empty cells
 empty = [[not grid[i][j] for i in range(9)] for j in range(9)]
 
-print(empty)
-
-# print(grid)
-
 #draw the game grid
 drawer.draw_grid(grid)
 
 #solve this puzzle!
 sol = solver.Solver(grid).get_solution()
-
-# print(sol)
 
 #check grid is valid
 if not checkers.check_valid(grid):
