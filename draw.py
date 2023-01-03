fc221924c94e044456d1b678df707509d5bfd600
Ivan Eremin <ereminiu@gmail.com>
2023-01-03 11:54:03 +0300

    Add typing for draw.py

--- draw.py
+++ draw.py
@@ -1,5 +1,6 @@
 import pygame as pg
 from tools import cols
+from typing import List, Optional
 
 class Drawer:
     def __init__(self, screen):
@@ -9,25 +10,25 @@ class Drawer:
         self.d, self.N = 30, 9
         self.font = pg.font.Font(None, self.fontsize)
     
-    def draw_text(self, text, x=0, y=0, color='red'):
+    def draw_text(self, text: str, x=0, y=0, color='red') -> None:
         tx = self.font.render(text, True, cols[color])
         self.screen.blit(tx, (x, y))
         pg.display.flip()
     
-    def fx(self, i=0):
+    def fx(self, i=0) -> int:
         return self.sx + self.d * i
     
-    def fy(self, i=0):
+    def fy(self, i=0) -> int:
         return self.sy + self.d * i 
     
-    def draw_tiles(self):
+    def draw_tiles(self) -> None:
         for i in range(10):
             pg.draw.line(self.screen, cols['black'], (self.fx(i), self.sy), (self.fx(i), self.fy(self.N)), 1 + (i%3 == 0))
             pg.draw.line(self.screen, cols['black'], (self.sx, self.fy(i)), (self.fx(self.N), self.fy(i)), 1 + (i%3 == 0))
         
         pg.display.flip()
     
-    def draw_grid(self, grid):
+    def draw_grid(self, grid: List[List[int]]) -> None:
         for i in range(9):
             for j in range(9):
                 val = grid[j][i]
@@ -36,7 +37,7 @@ class Drawer:
 
         pg.display.flip()
 
-    def draw_diff(self, ans, empty):
+    def draw_diff(self, ans: List[List[int]], empty: List[List[int]]) -> None:
         for i in range(9):
             for j in range(9):
                 if empty[i][j]:
